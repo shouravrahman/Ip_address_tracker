dfbc68dce8a45eda14a573de3a9ca4066b438d61
shouravrahman006 <shouravrahman006@gmail.com>
2021-05-30 00:33:07 +0600

    made the effect work and send response

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 // import { useFetch } from './useFetch';
-// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import GlobalStyle from './globalStyles';
 
+const API_KEY = 'at_xtpIidIz9vUqEzlBODaUwtChmVRXf';
+// const URL = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ip}`;
+
 function App() {
+	const [ip, setIp] = useState('');
+	const [response, setResponse] = useState(null);
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+	useEffect(() => {
+		const fetchData = async () => {
+			setIsLoading(true);
+			try {
+				const res = await fetch(
+					`https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ip}`
+				);
+				const json = await res.json();
+				setResponse(json);
+				setIsLoading(false);
+			} catch (error) {
+				setError(error);
+			}
+		};
+		fetchData();
+		console.log(response);
+	}, [ip]);
 	return (
 		<>
 			<GlobalStyle />
-			<Header input={input} />
+			<Header getIp={(q) => setIp(q)} />
 		</>
 	);
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,35 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 import React from 'react';
 import { Form, FormInput, Head, HeadLine, Button } from './Header.elements';
 // import { useFetch } from '../../useFetch';
 
-const Header = () => {
+const Header = ({ getIp }) => {
+	const [input, setInput] = useState('');
+
+	const onChange = (q) => {
+		setInput(q);
+		getIp(q);
+	};
 	return (
 		<>
 			{/* styled components */}
 			<Head>
 				<HeadLine>IP Address Tracker</HeadLine>
-				<Form>
-					<FormInput placeholder='Search for any IP address or Domain' />
+				<Form
+					onSubmit={(e) => {
+						e.preventDefault();
+						onChange(input);
+						setInput('');
+					}}
+				>
+					<FormInput
+						value={input}
+						onChange={(e) => {
+							setInput(e.target.value);
+						}}
+						placeholder='Search for any IP address or Domain'
+					/>
 					<Button type='submit'>
 						<FaArrowRight />
 					</Button>
